e3c67f052fe577f093b6ec91d8deb8f25ce41223
Boyan Ferdinandov <chopperbobber963@gmail.com>
2026-01-07 23:46:46 +0200

    User Settings Updated

---
+++ SkillsBarter/SkillsBarter/Migrations/20260107204658_AddUserPreferencesAndEmailChange.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkillsBarter.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPreferencesAndEmailChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailChangeToken",
+                table: "users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EmailChangeTokenExpiry",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PendingEmail",
+                table: "users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PreferencesJson",
+                table: "users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailChangeToken",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "EmailChangeTokenExpiry",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "PendingEmail",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "PreferencesJson",
+                table: "users");
+        }
+    }
+}
